9be1b7796ca9e83ba66e00f70ccff4b08df0fd5c
TheFatAndFurious <mrguerrilla@gmail.com>
2024-05-17 11:02:49 +0200

    loggin to win

--- internal/handlers/handlersConfig.go
+++ internal/handlers/handlersConfig.go
@@ -3,6 +3,7 @@ package handlers
 
 import (
 	"log"
+	"os"
 
 	"goserver.com/internal/utils"
 )
@@ -11,7 +12,20 @@ var basePath string
 
 // InitializeHandlers sets up necessary configurations for handlers
 func InitializeHandlers() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Failed to get current working directory: %v", err)
+	}
+	log.Printf("Current working directory: %s", cwd)
 	relativePath := utils.GetEnv("TEMPLATES_PATH", "web/templates/")
+	log.Printf("TEMPLATES_PATH: %s", relativePath)
 	basePath = utils.GetAbsolutePath(relativePath)
 	log.Printf("Template path set to: %s", basePath)
+	files, err := os.ReadDir(basePath)
+	if err != nil {
+		log.Fatalf("Failed to read directory: %v", err)
+	}
+	for _, file := range files {
+		log.Printf("Found file: %s", file.Name())
+	}
 }
